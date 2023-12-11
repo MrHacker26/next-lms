ca7a0af3e81c195c70efd7fa6af08cfac399a86f
tarun268 <tarun@prodios.com>
2023-12-11 11:53:16 +0530

    feat(courses): add create course page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
 const CoursesPage = () => {
-  return <div>Courses Page</div>
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>Create course</Button>
+      </Link>
+    </div>
+  )
 }
 
 export default CoursesPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ToastProvider } from '@/components/providers/toaster-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
