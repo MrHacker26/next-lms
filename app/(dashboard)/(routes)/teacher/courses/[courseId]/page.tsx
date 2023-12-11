2fc39cbb838c6426611e01afd272fef463080604
tarun268 <tarun@prodios.com>
2023-12-11 17:06:29 +0530

    feat(courses): add form field to update title of the course

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { LayoutDashboard } from 'lucide-react'
 
 import { db } from '@/lib/db'
 import { IconBadge } from '@/components/icon-badge'
+import { TitleForm } from './title-form'
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth()
@@ -44,6 +45,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <IconBadge icon={LayoutDashboard} />
               <h2 className="text-xl">Customize your course</h2>
             </div>
+            <TitleForm initialData={course} courseId={course.id} />
           </div>
         </div>
       </div>
