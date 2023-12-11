798da7337b9323fb33163275be645347c1c05364
tarun268 <tarun@prodios.com>
2023-12-11 16:18:45 +0530

    feat(courses): create courses edit page ui

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,54 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { LayoutDashboard } from 'lucide-react'
+
+import { db } from '@/lib/db'
+import { IconBadge } from '@/components/icon-badge'
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  })
+
+  if (!course) {
+    return redirect('/')
+  }
+
+  const requiredFields = [course.title, course.description, course.imageUrl, course.price, course.categoryId]
+
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+
+  const completionText = `(${completedFields}/${totalFields})`
+
+  return (
+    <>
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">Course setup</h1>
+            <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+          </div>
+        </div>
+        <div className="mt-16 grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customize your course</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CourseIdPage
---
+++ app/api/courses/route.ts
@@ -0,0 +1,25 @@
+import { auth } from '@clerk/nextjs'
+import { NextRequest, NextResponse } from 'next/server'
+import { db } from '@/lib/db'
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId } = auth()
+    const { title } = await request.json()
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const course = await db.course.create({
+      data: {
+        title,
+        createdById: userId,
+      },
+    })
+
+    return NextResponse.json(course)
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
