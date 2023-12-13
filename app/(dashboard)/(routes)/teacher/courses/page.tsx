85512efca62620d53ca1b716cc5973ad5d1b7d80
tarun268 <tarun@prodios.com>
2023-12-13 14:45:45 +0530

    feat(courses): add table to show courses list

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,23 @@
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
+import { DataTable } from './_component/data-table'
+import { columns } from './_component/columns'
+// import { DataTable } from './_components/data-table'
+// import { columns } from './_components/columns'
 
-const CoursesPage = () => {
+export default async function Courses() {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const courses = await db.course.findMany({ where: { createdById: userId }, orderBy: { createdAt: 'desc' } })
+
   return (
-    <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>Create course</Button>
-      </Link>
+    <div className="space-y-6 p-6">
+      <DataTable columns={columns} data={courses} />
     </div>
   )
 }
-
-export default CoursesPage
